357940bc50ae2e7b820ab42bc0b2a9db4b83ad4c
MingZ266 <mingzheng266@outlook.com>
2021-06-05 09:31:09 +0800

    修复因重启等原因致使小窗口更新带来数据丢失的问题

--- app/src/main/java/com/mingz/desktopnotes/widget/NotesWidget.java
+++ app/src/main/java/com/mingz/desktopnotes/widget/NotesWidget.java
@@ -13,10 +13,18 @@ import androidx.annotation.IntDef;
 
 import com.mingz.desktopnotes.R;
 import com.mingz.desktopnotes.activity.EditTextActivity;
+import com.mingz.desktopnotes.utils.MyLog;
 
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.nio.charset.StandardCharsets;
 
 /**
  * App Widget 功能的实现.
@@ -42,18 +50,18 @@ public class NotesWidget extends AppWidgetProvider {
                 appWidgetId, openEdit, PendingIntent.FLAG_UPDATE_CURRENT));
         // 指示小部件管理器更新小部件
         appWidgetManager.updateAppWidget(appWidgetId, views);
+        widgetAttr.toJSON(context, appWidgetId);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        WidgetAttr widgetAttr = new WidgetAttr();
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId, widgetAttr);
+            updateAppWidget(context, appWidgetManager, appWidgetId, WidgetAttr.getInstance(context, appWidgetId));
         }
     }
 
     public static class WidgetAttr implements Serializable {
-        //private final MyLog myLog = new MyLog("WidgetAttrMyTAG");
+        // 公开属性值
         /**
          * 透明.
          */
@@ -89,19 +97,80 @@ public class NotesWidget extends AppWidgetProvider {
          */
         public static final int SIZE_MAX = 45;
 
-        private static final long serialVersionUID = 202106031218L;
+        // 内部属性
         private int backgroundResId;// 背景资源id
         private int backgroundColor;// 背景颜色
         private String notes;// 便签内容
         private int textColor;// 文本颜色
         private int textSize;// 文本字体大小（sp）
 
-        public WidgetAttr() {
+        // 其它辅助属性
+        private static final MyLog myLog = new MyLog("WidgetAttrMyTAG");
+        private static final long serialVersionUID = 202106031218L;
+        private static final String JSON_BG_RES_ID = "backgroundResId";
+        private static final String JSON_BG_COLOR = "backgroundColor";
+        private static final String JSON_NOTES = "notes";
+        private static final String JSON_TEXT_COLOR = "textColor";
+        private static final String JSON_TEXT_SIZE = "textSize";
+
+        // 使用默认值构造
+        private WidgetAttr() {
             backgroundResId = R.drawable.bg_widget_half_alpha;
             this.backgroundColor = Color.argb(255, 255, 255, 255);
             this.notes = "桌面便签";
             this.textColor = Color.GRAY;
             this.textSize = SIZE_MIDDLE;
+        }
+
+        // 从读取的文件内容构造
+        private WidgetAttr(int backgroundResId, int backgroundColor, String notes,
+                           int textColor, int textSize) {
+            this.backgroundResId = backgroundResId;
+            this.backgroundColor = backgroundColor;
+            this.notes = notes;
+            this.textColor = textColor;
+            this.textSize = textSize;
+        }
+
+        /**
+         * 获取一个实例.<br>
+         * 该实例从已保存的文件构造，若无保存的文件
+         * 或文件解析异常则使用默认值构造.
+         *
+         * @param context 上下文
+         * @param appWidgetId 视窗id
+         * @return 构造的实例
+         */
+        public static WidgetAttr getInstance(Context context, int appWidgetId) {
+            File cache = new File(context.getFilesDir(), "widget_" + appWidgetId);
+            if (cache.exists()) {
+                try (FileInputStream fis = new FileInputStream(cache)) {
+                    byte[] data = new byte[1024];// 1KB
+                    int len;// 读取长度
+                    StringBuilder input = new StringBuilder();// 读取内容
+                    while ((len = fis.read(data)) != -1) {
+                        input.append(new String(data, 0, len, StandardCharsets.UTF_8));
+                    }
+                    try {
+                        JSONObject jsonObject = new JSONObject(input.toString());
+                        return new WidgetAttr(
+                                Integer.parseInt(jsonObject.getString(JSON_BG_RES_ID)),
+                                Integer.parseInt(jsonObject.getString(JSON_BG_COLOR)),
+                                jsonObject.getString(JSON_NOTES),
+                                Integer.parseInt(jsonObject.getString(JSON_TEXT_COLOR)),
+                                Integer.parseInt(jsonObject.getString(JSON_TEXT_SIZE))
+                        );
+                    } catch (Exception e) {
+                        myLog.w("错误的文件内容（" + cache.getName() + "): " + input.toString());
+                        if (!cache.delete()) {
+                            myLog.w("文件删除失败");
+                        }
+                    }
+                } catch (IOException e) {
+                    return new WidgetAttr();
+                }
+            }
+            return new WidgetAttr();
         }
 
         /**
@@ -152,6 +221,31 @@ public class NotesWidget extends AppWidgetProvider {
                     (!this.notes.equals(notes)) ||
                     this.textColor != textColor ||
                     this.textSize != textSize;
+        }
+
+        /**
+         * 将所属对象以json形式存储于文件中.
+         *
+         * @param context 上下文
+         * @param appWidgetId 视窗id
+         */
+        public void toJSON(Context context, int appWidgetId) {
+            File cache = new File(context.getFilesDir(), "widget_" + appWidgetId);
+            try {
+                if (cache.exists() || cache.createNewFile()) {
+                    try (FileOutputStream fos = new FileOutputStream(cache)) {
+                        String output = "{\"" + JSON_BG_RES_ID + "\":\"" + backgroundResId +
+                                "\",\"" + JSON_BG_COLOR + "\":\"" + backgroundColor +
+                                "\",\"" + JSON_NOTES + "\":\"" + notes +
+                                "\",\"" + JSON_TEXT_COLOR + "\":\"" + textColor +
+                                "\",\"" + JSON_TEXT_SIZE + "\":\"" + textSize +
+                                "\"}";// 存储内容
+                        fos.write(output.getBytes(StandardCharsets.UTF_8));
+                    }
+                }
+            } catch (IOException e) {
+                myLog.w("将WidgetAttr写入文件异常", e);
+            }
         }
 
         public int getAlphaLevel() {
